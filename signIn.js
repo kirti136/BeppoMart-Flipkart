912c6e1e2e564fd96279ede3be294ebb3eda5a8e
kirti136 <bhosalekirti2002@gmail.com>
2022-12-20 13:01:00 +0530

    project file updated again

--- signIn.js
+++ signIn.js
@@ -11,3 +11,10 @@ document.querySelectorAll("#close").forEach(n => n.addEventListener("click", () 
     hamburger.classList.remove("active");
     navMenu.classList.remove("active");
 }))
+
+
+let button = document.getElementById("sign-button");
+
+button.addEventListener("click",()=>{
+    alert("OTP shared on your mobile")
+});
